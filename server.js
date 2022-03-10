1cca888dcd63eefc6b7fa51c9a44336315248ee5
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-03-10 10:05:53 +0900

    updated directories

--- server.js
+++ server.js
@@ -21,18 +21,11 @@ const image = require('./controllers/image');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
-
+app.get('/', (req, res) => {res.send("It's working!")})
 app.post('/signin', (req, res) => {signin.handleSingin(req, res, knex, bcrypt)})
-
 app.post('/register', (req, res) => {register.handleRegister(req, res, knex, bcrypt)})
-
 app.get('/profile/:id', (req, res) => {profile.handleProfileGET(req, res, knex)})
-
 app.put('/image', (req, res) => {image.handleImage(req, res, knex)})
-
-app.listen(3000, () => {
-    console.log("app is running");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on ${process.env.PORT}`);
 });
