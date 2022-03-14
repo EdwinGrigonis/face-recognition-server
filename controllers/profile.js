4c1463ef43e575a775b29cf52eb6404db9968f15
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-03-14 10:05:36 +0900

    replacing files

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,16 @@
-const handlerProfileGET = (req, res, knex) => {
-    const { id } = req.params;
-    knex.select('*').from('users').where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(404).json('Not found');
-            }
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
     })
-    .catch(err => res.status(400).json('Unable to get user'));
+    .catch(err => res.status(400).json('error getting user'))
 }
 
 module.exports = {
-    handlerProfileGET: handlerProfileGET
-};
+  handleProfileGet
+}
